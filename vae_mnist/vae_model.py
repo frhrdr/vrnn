a823302e3165c3810b6a084171edbb4d66869959
Fr-d-rik <frederikharder@gmail.com>
2016-09-14 14:43:32 +0200

    buggy VAE. can run training but results are crap

--- vae_mnist/vae_model.py
+++ vae_mnist/vae_model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import math
 
+
 def simple_mlp(input_tensor, n_in, n_hid, n_out):
     # hidden layer
     with tf.name_scope('hidden'):
@@ -24,7 +25,7 @@ def simple_mlp(input_tensor, n_in, n_hid, n_out):
     return out_tensor
 
 
-def inference(images, encoder_nn=None, decoder_nn=None, n_latentvars=10):
+def inference(images, batch_size, encoder_nn=None, decoder_nn=None, n_latentvars=10):
 
     # make encoder if not provided
     if encoder_nn is None:
@@ -32,38 +33,53 @@ def inference(images, encoder_nn=None, decoder_nn=None, n_latentvars=10):
             encoder_nn = simple_mlp(images, 28**2, 128, 2*n_latentvars)
 
     # create mean and covariance
-    mean_vec = tf.slice(encoder_nn, [0], [n_latentvars], name='mu')
-    cov_vec = tf.slice(encoder_nn, [n_latentvars], [n_latentvars])
-    cov_mat = tf.batch_matrix_diag(cov_vec, name='Sigma')
+
+    # batch_size = encoder_nn.get_shape() (How to do this properly??)
+    mean_vec = tf.slice(encoder_nn, [0, 0], [batch_size, n_latentvars], name='mu')
+    cov_vec = tf.slice(encoder_nn, [0, n_latentvars], [batch_size, n_latentvars], name='Sigma')
+    # cov_mat = tf.batch_matrix_diag(cov_vec)
 
     # draw samples
-    noise_vec = tf.random_normal(n_latentvars, mean=0.0, stddev=1.0, dtype=tf.float32, seed=123, name='z_noise')
+    noise_vec = tf.random_normal([n_latentvars], mean=0.0, stddev=1.0, dtype=tf.float32, seed=123, name='z_noise')
     z_vec = mean_vec + tf.mul(cov_vec, noise_vec)
 
     # make decoder if not provided
     if decoder_nn is None:
-        with tf.name_scope('encoder'):
+        with tf.name_scope('decoder'):
             decoder_nn = simple_mlp(z_vec, n_latentvars, 128, 28**2)
 
-    return decoder_nn, mean_vec, cov_mat
+    return decoder_nn, mean_vec, cov_vec
 
-def bound(vae, mean_vec, cov_mat, target):
+
+def loss(vae, mean_vec, cov_vec, target, n_latentvars):
 
     # KL divergence (see VAE Tut formula 7)
     # trace and determinant could be optimized.
-    k = tf.to_double(mean_vec.shape[0])
-    kl_div = tf.div(tf.trace(cov_mat) + tf.matmul(mean_vec, mean_vec, transpose_a=True) -
-                    k + tf.log(tf.det(cov_mat)), 2, name='KL-divergence')
+    # k = tf.to_double(mean_vec.get_shape[0]) (How??)
+    k = tf.to_float(n_latentvars)
+    cov_trace = tf.reduce_sum(cov_vec, reduction_indices=[1], name='l1')
+    cov_determinant = tf.reduce_prod(cov_vec, reduction_indices=[1], name='l2')
+    mean_squared = tf.reduce_sum(tf.mul(mean_vec, mean_vec, name='l3'), reduction_indices=[1], name='l4')
 
+    kl_div = tf.div(tf.sub(tf.add(cov_trace, mean_squared, name='l5'),
+                    tf.add(k, tf.log(cov_determinant + tf.to_float(0.0000001), name='l6'), name='l7'), name='l8'),
+                    tf.to_float(2), name='KL-divergence')
+
+    print("kldiv: ", kl_div.get_shape())
     # reconstruction error
     diff = target - vae
-    rec_err = tf.matmul(diff, diff, transpose_a=True, name='reconstruction error')
+    print("diff: ", diff.get_shape())
+    rec_err = tf.reduce_prod(tf.mul(diff, diff, name='l9'), reduction_indices=[1], name='rec_err')
 
+    # so the error happens somewhere in the computation of the rec_err gradient.
+    # both the neg_lower_bound (duh) and the KL-divergence gradients come before (not l8 though) and seem fine
 
     # negative variational lower bound
     # (optimizer can only minimize - same as maximizing positive lower bound
-    bound = tf.add(kl_div, rec_err, name='neg lower bound')
+    bound = tf.add(kl_div, rec_err, name='neg_lower_bound')
+    print("bound: ", bound.get_shape())
     return bound
+
 
 def training(bound, learning_rate):
 
---
+++ vae_mnist/vae_train.py
@@ -0,0 +1,110 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import time
+import os.path
+import vae_model
+from tensorflow.examples.tutorials.mnist import input_data
+
+# flags = tf.app.flags
+# FLAGS = flags.FLAGS
+# flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
+# flags.DEFINE_integer('max_steps', 2000, 'Number of steps to run trainer.')
+# flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')
+# flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
+# flags.DEFINE_integer('batch_size', 100, 'Batch size.  '
+#                      'Must divide evenly into the dataset sizes.')
+# flags.DEFINE_string('train_dir', 'data', 'Directory to put the training data.')
+# flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data '
+#                      'for unit testing.')
+
+max_steps = 505
+train_dir = 'data'
+batch_size = 100
+# data_sets = input_data.read_data_sets('data', False)
+# print(data_sets)
+# print(data_sets.train.next_batch(5))
+
+
+def placeholder_inputs(batch_size):
+    images_placeholder = tf.placeholder(tf.float32, shape=(batch_size, 28**2))
+    target_placeholder = tf.placeholder(tf.float32, shape=(batch_size, 28**2))
+    return images_placeholder, target_placeholder
+
+
+def fill_feed_dict(data_set, images_pl, target_pl):
+    images_feed, _ = data_set.next_batch(batch_size)
+
+    feed_dict = {
+        images_pl: images_feed,
+        target_pl: images_feed,
+    }
+    return feed_dict
+
+
+def run_training():
+    data_sets = input_data.read_data_sets(train_dir)
+
+    with tf.Graph().as_default():
+        images_placeholder, target_placeholder = placeholder_inputs(batch_size)
+
+        vae, mean_vec, cov_vec = vae_model.inference(images_placeholder, batch_size, n_latentvars=10)
+        bound = vae_model.loss(vae, mean_vec, cov_vec, target_placeholder, n_latentvars=10)
+        train_op = vae_model.training(bound, learning_rate=0.1)
+
+        summary_op = tf.merge_all_summaries()
+        saver = tf.train.Saver()
+
+        sess = tf.Session()
+        summary_writer = tf.train.SummaryWriter('data', sess.graph)
+        sess.run(tf.initialize_all_variables())
+
+        for step in xrange(max_steps):
+
+            start_time = time.time()
+
+            feed_dict = fill_feed_dict(data_sets.train,
+                                       images_placeholder,
+                                       target_placeholder)
+
+            _, loss_value = sess.run([train_op, bound],
+                                     feed_dict=feed_dict)
+
+            duration = time.time() - start_time
+
+            if step % 100 == 0:
+                print(loss_value.shape)
+                l = np.sum(loss_value)
+                print('Step %d: loss = %.2f (%.3f sec)' % (step, l, duration))
+                # Update the events file.
+                #summary_str = sess.run(summary_op, feed_dict=feed_dict)
+                #summary_writer.add_summary(summary_str, step)
+                #summary_writer.flush()
+
+            # maybe later
+            # if (step + 1) % 1000 == 0 or (step + 1) == max_steps:
+            #     checkpoint_file = os.path.join(train_dir, 'checkpoint')
+            #     saver.save(sess, checkpoint_file, global_step=step)
+            #     # Evaluate against the training set.
+            #     print('Training Data Eval:')
+            #     do_eval(sess,
+            #             eval_correct,
+            #             images_placeholder,
+            #             target_placeholder,
+            #             data_sets.train)
+            #     # Evaluate against the validation set.
+            #     print('Validation Data Eval:')
+            #     do_eval(sess,
+            #             eval_correct,
+            #             images_placeholder,
+            #             target_placeholder,
+            #             data_sets.validation)
+            #     # Evaluate against the test set.
+            #     print('Test Data Eval:')
+            #     do_eval(sess,
+            #             eval_correct,
+            #             images_placeholder,
+            #             target_placeholder,
+            #             data_sets.test)
+
+run_training()
