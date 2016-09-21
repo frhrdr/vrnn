ca271e810320c49dd3a1c5d185afbb933f5f13c7
Fr-d-rik <frederikharder@gmail.com>
2016-09-21 19:55:49 +0200

    started building netgen

--- utilities.py
+++ utilities.py
@@ -3,28 +3,80 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+class NetGen:
+
+    def __init__(self):
+        self.fd = {}
+
+    def __str__(self):
+        return str(self.fd.keys())
+
+    # adds net-gen function of certain type to the internal dict
+    def add_net(self, name, nn_type, params):
+
+        if nn_type is 'simple_mlp':
+            n_in, n_hid, n_out = params['layers']
+
+            def f(in_pl):
+                return simple_mlp(in_pl, n_in, n_hid, n_out, name)
+            self.fd[name] = f
+
+    # concatenates several tensors into one input to existing nn of given name
+    def weave_inputs(self, name, in_dims):
+        f = self.fd[name]
+
+        def g(*args):
+            # TODO: maybe make dimension flexible
+            in_pl = tf.concat(1, list(args), name=name + "_joint_inputs")
+            return f(in_pl)
+
+        self.fd[name] = g
+
+
 def simple_mlp(input_tensor, n_in, n_hid, n_out, scope):
     # hidden layer
     with tf.name_scope(scope):
         with tf.name_scope('hidden'):
-            weights = tf.Variable(
-                tf.truncated_normal([n_in, n_hid],
-                                    stddev=1.0 / math.sqrt(float(n_in))),
-                name='weights')
-            biases = tf.Variable(tf.zeros([n_hid]),
-                                 name='biases')
+            weights = tf.Variable(tf.truncated_normal([n_in, n_hid],
+                                  stddev=1.0 / math.sqrt(float(n_in))),
+                                  name='weights')
+            biases = tf.Variable(tf.zeros([n_hid]), name='biases')
             hidden1 = tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
         # output layer
         with tf.name_scope('output'):
-            weights = tf.Variable(
-                tf.truncated_normal([n_hid, n_out],
-                                    stddev=1.0 / math.sqrt(float(n_hid))),
-                name='weights')
-            biases = tf.Variable(tf.zeros([n_out]),
-                                 name='biases')
+            weights = tf.Variable(tf.truncated_normal([n_hid, n_out],
+                                  stddev=1.0 / math.sqrt(float(n_in))),
+                                  name='weights')
+            biases = tf.Variable(tf.zeros([n_out]), name='biases')
             out_tensor = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
 
     return out_tensor
+
+
+def general_mlp(input_tensor, layers, name):
+
+    n_hidden_layers = len(layers) - 2
+    last = input_tensor
+    with tf.name_scope(name):
+        # stack hidden layers
+        for idx in range(n_hidden_layers):
+            with tf.name_scope('hidden' + str(idx + 1)):
+
+                weights = tf.Variable(tf.truncated_normal([layers[idx], layers[idx+1]],
+                                      stddev=1.0 / math.sqrt(float(layers[idx]))),
+                                      name='weights')
+                biases = tf.Variable(tf.zeros([layers[idx+1]]), name='biases')
+                last = tf.nn.relu(tf.matmul(last, weights) + biases)
+
+        # make output layer
+        with tf.name_scope('out'):
+            weights = tf.Variable(tf.truncated_normal([layers[idx], layers[idx+1]],
+                                  stddev=1.0 / math.sqrt(float(layers[idx]))),
+                                  name='weights')
+            biases = tf.Variable(tf.zeros([layers[idx+1]]), name='biases')
+            out = tf.nn.relu(tf.matmul(last, weights) + biases)
+    return out
 
 
 def plot_img_mats(mat):
--- vrnn_test/vrnn_model.py
+++ vrnn_test/vrnn_model.py
@@ -1,14 +1,38 @@
 import tensorflow as tf
 import math
 
-def inference():
-    pass
+
+def inference(in_pl, hid_pl, param_dict, fun_dict):
+
+    pd = param_dict
+    fd = fun_dict
+
+    phi_x = fd['phi_x'](in_pl)
+
+    phi_enc = fd['phi_enc'](phi_x, hid_pl)
+    mean_x = tf.slice(phi_enc, [0, 0], [pd['batch_size'], pd['n_latent']], name='mean_x')
+    cov_x = tf.slice(phi_enc, [0, pd['n_latent']], [pd['batch_size'], pd['n_latent']], name='cov_x')
+
+    phi_prior = fd['phi_prior'](hid_pl)
+    mean_0 = tf.slice(phi_prior, [0, 0], [pd['batch_size'], pd['n_latent']], name='mean_0')
+    cov_0 = tf.slice(phi_prior, [0, pd['n_latent']], [pd['batch_size'], pd['n_latent']], name='cov_0')
+
+    noise = tf.random_normal([pd['n_latent']], mean=0.0, stddev=1.0, dtype=tf.float32, seed=123, name='z_noise')
+    z = tf.add(mean_x, tf.mul(cov_x, noise), name='z_vec')
+
+    phi_z = fd['phi_z'](z)
+    phi_dec = fd['phi_dec'](phi_z, hid_pl)
+
+    f_theta = fd['f_theta'](hid_pl, phi_x, phi_z)
+
 
 def loss():
     pass
 
+
 def train():
     pass
+
 
 def generate():
     pass
