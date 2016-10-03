a4b52de763ad8851a1e221d3d27c8ec50a299a90
Fr-d-rik <frederikharder@gmail.com>
2016-10-03 17:17:29 +0200

    number series 2 ready. getting ready for testing...

--- vrnn_test/number_series_gen.py
+++ vrnn_test/number_series_gen.py
@@ -55,7 +55,63 @@ def series1_check(series):
 
     return score / np.sum(score)
 
-path = 'data/series1_1000_by_300.npy'
-save_series(series1_gen, 1000, 300, path)
-# s = load_series(path)
-# print(s)
+
+def series2_gen(length, dim):
+    # starts off as array of zeros.
+    # 0 -> 1 rules:
+    # every cell has p=0.1 of switching to 1, when 0.
+    # p=0.5/0.9 with one/two neighbours that are 1.
+    # 1 -> 0 rules:
+    # p=0.2 in general
+    # p=0.4/0.6 with one or two of the leftmost neighbours active
+
+    # lacks longer term dependencies. but maybe good enough for now
+    series = np.zeros((length, dim))
+    for t in range(1, length):
+        timestep = np.zeros((dim,))
+        for idx in range(dim):
+            if series[t-1, idx] == 0:
+                odds = 0.1
+                if idx > 0 and series[t-1, idx-1] == 1: odds += 0.4
+                if idx < dim -1 and series[t-1, idx+1] == 1: odds += 0.4
+                if odds > np.random.uniform(): series[t, idx] = 1
+            else:
+                odds = 0.2
+                if idx > 0 and series[t-1, idx-1] == 1: odds += 0.2
+                if idx > 1 and series[t-1, idx-2] == 1: odds += 0.2
+                if odds < np.random.uniform(): series[t, idx] = 1
+    return series
+
+
+# print series2_gen(30, 17)
+def series2_check(series):
+    # counts rule applications: 6 rules
+    # 0->1:  0) p=0.1  1) p=0.5  2) p=0.9
+    # 1->0:  3) p=0.2  4) p=0.4  5) p=0.6
+    # choice in dimension 2: 0) picked 0, 1) picked 1)
+    score = np.zeros((6, 2), dtype=np.int)
+    series = series.round().astype(np.int32)
+    length = series.shape[0]
+    dim = series.shape[1]
+    for t in range(1, length):
+        timestep = np.zeros((dim,))
+        for idx in range(dim):
+            choice = series[t, idx]
+            if series[t-1, idx] == 0:
+                rule = 0
+                if idx > 0 and series[t-1, idx-1] == 1: rule += 1
+                if idx < dim - 1 and series[t-1, idx+1] == 1: rule += 1
+                score[rule, choice] += 1
+            else:
+                rule = 3
+                if idx > 0 and series[t-1, idx-1] == 1: rule += 1
+                if idx > 1 and series[t-1, idx-2] == 1: rule += 1
+                score[rule, choice] += 1
+
+    # return measured probabilities of 0 in each case. should be around [0.9 0.5 0.1  0.2 0.4 0.6]
+    return score[:, 0].astype(np.float32) / np.sum(score, 1).astype(np.float32)
+
+s = series2_gen(200, 100)
+print(s)
+c = series2_check(s)
+print(c)
