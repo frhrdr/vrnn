b7fdd9f151a53a13449f7c0ed7db86db8ac76b30
Fr-d-rik <frederikharder@gmail.com>
2017-02-15 18:07:36 +0100

    handwriting data prepped

--- vrnn_test/iamondb_reader.py
+++ vrnn_test/iamondb_reader.py
@@ -1,9 +1,11 @@
 from __future__ import print_function
+import os
+import fnmatch
 import xml.etree.ElementTree as ET
 import numpy as np
 import matplotlib.pyplot as plt
 
-def xml_to_mat(xml_path, interpolate=False):
+def xml_to_mat(xml_path, interpolate=False, max_dist=300):
 
     if interpolate:
         raise NotImplementedError
@@ -27,34 +29,70 @@ def xml_to_mat(xml_path, interpolate=False):
     mat[1:, :2] = mat[1:, :2] - mat[:-1, :2]
     mat[0, :2] = 0
 
+    mat = np.maximum(mat, - max_dist)
+    mat = np.minimum(mat, max_dist)
     return mat
 
 
-def mat_to_img(mat):
-    mat = mat[:, :2]
+def mat_to_plot(mat):
+
     for idx in range(2, mat.shape[0]):
-        mat[idx, :] = mat[idx, :] + mat[idx - 1, :]
+        mat[idx, :2] = mat[idx, :2] + mat[idx - 1, :2]
 
-    xy_min = np.min(mat, axis=0)
+    mat[:, 1] = - mat[:, 1]  # flip y axis for accurate plot
 
-    mat[:, 0] = mat[:, 0] - xy_min[0]
-    mat[:, 1] = mat[:, 1] - xy_min[1]
+    stroke_ends = np.argwhere(mat[:, 2])  # single out individual strokes
+    begin = 0
+    for end in stroke_ends:
+        end = int(end)
+        plt.axis('equal')
+        plt.plot(mat[begin:end, 0], mat[begin:end, 1], c='blue')
+        begin = end + 1
+    plt.show()
 
-    mat = mat / 10
 
-    xy_max = np.max(mat, axis=0)
-    image = np.ones(xy_max+1, dtype=int)
+def parse_data_set(target_dir, root_dir='data/handwriting/xml_data_root/lineStrokes'):
+    # for file in os.listdir(root_dir):
+    #     if file.endswith('.xml'):
+    #         print(file)
+    #         break
+    matches = []
+    for root, dirnames, filenames in os.walk(root_dir):
+        for filename in fnmatch.filter(filenames, '*.xml'):
+            matches.append(os.path.join(root, filename))
+    print(len(matches))
+    print(matches[0])
 
-    for idx in range(mat.shape[0]):
-        image[mat[idx, 0], mat[idx, 1]] = 0
-        pass
+    mat_list = []
+    for file in matches:
+        mat = xml_to_mat(file)
+        mat_list.append(mat)
+        if len(mat_list) % 100 == 0:
+            print('loaded ' + str(len(mat_list)) + '/' + str(len(matches)) + ' files')
 
-    im = plt.imshow(image.T, cmap='gray')
-    plt.show(im)
+    len_list = [k.shape[0] for k in mat_list]
+    sequence_indices = np.asarray(len_list)
+    sequences = np.concatenate(mat_list, axis=0)
+    np.save(target_dir + '/sequence_indices.npy', sequence_indices)
+    np.save(target_dir + '/sequences.npy', sequences)
+
+    plt.hist(len_list, 50, normed=1, facecolor='green', alpha=0.75)
+    plt.show()
+
+
+def load_sequences(source_dir, seq_file='sequences.npy', idx_file='sequence_indices.npy'):
+    seq_mat = np.load(source_dir + '/' + seq_file)
+    idx_mat = np.load(source_dir + '/' + idx_file)
+    plt.hist(idx_mat, 50, normed=1, facecolor='green', alpha=0.75)
+    plt.show()
+    for idx in range(1, idx_mat.shape[0]):
+        idx_mat[idx] = idx_mat[idx] + idx_mat[idx - 1]
 
 # a01-000u-01
-a = xml_to_mat('data/handwriting/strokesu.xml')
-mat_to_img(a)
+# a = xml_to_mat('data/handwriting/strokesu.xml')
+# mat_to_plot(a)
+# parse_data_set('data/handwriting')
+load_sequences('data/handwriting')
 
 
 # to save:
