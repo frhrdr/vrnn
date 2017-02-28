db50b6804e1ecd61f856d258801abcb4af88adee
Fr-d-rik <frederikharder@gmail.com>
2017-02-28 13:49:14 +0100

    moving utilities

--- vrnn_test/params.py
+++ vrnn_test/params.py
@@ -3,9 +3,9 @@ PARAM_DICT = dict()
 PARAM_DICT['watchlist'] = {'allmc': []}
 
 # data path
-PARAM_DICT['series'] = 7
-PARAM_DICT['data_path'] = 'data/handwriting/rough_cut_500_pad_500_max_300_norm_xyonly.npy'
-PARAM_DICT['log_path'] = 'data/logs/handwriting_08'
+PARAM_DICT['series'] = -1
+PARAM_DICT['data_path'] = 'data/handwriting/rough_cut_200_pad_500_max_300_norm_xyonly.npy'
+PARAM_DICT['log_path'] = 'data/logs/handwriting_09'
 PARAM_DICT['log_freq'] = 1000
 PARAM_DICT['print_freq'] = 200
 
