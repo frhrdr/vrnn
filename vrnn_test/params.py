dd4fbba56f9b7fceda930eee8aa9ce7f930eb066
Fr-d-rik <frederikharder@gmail.com>
2017-03-06 15:08:17 +0100

    test 24 setup - small lstm

--- vrnn_test/params.py
+++ vrnn_test/params.py
@@ -5,7 +5,7 @@ PARAM_DICT['watchlist'] = {'allmc': []}
 # data path
 PARAM_DICT['series'] = -1
 PARAM_DICT['data_path'] = 'data/handwriting/rough_cut_200_pad_0_max_300_norm_xyonly.npy'
-PARAM_DICT['log_path'] = 'data/logs/handwriting_23'
+PARAM_DICT['log_path'] = 'data/logs/handwriting_24'
 PARAM_DICT['log_freq'] = 500
 PARAM_DICT['print_freq'] = 200
 
@@ -20,9 +20,9 @@ PARAM_DICT['batch_size'] = 100
 PARAM_DICT['data_dim'] = 2
 PARAM_DICT['n_latent'] = 200
 PARAM_DICT['seq_length'] = 200
-PARAM_DICT['learning_rate'] = 0.0001
+PARAM_DICT['learning_rate'] = 0.001
 PARAM_DICT['max_iter'] = 2000
-PARAM_DICT['hid_state_size'] = 2000
+PARAM_DICT['hid_state_size'] = 20
 PARAM_DICT['masking'] = False
 PARAM_DICT['mask_value'] = 500
 
