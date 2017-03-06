test 28 setup - fixed variance

vrnn_test/utilities.py
@@ -137,7 +137,7 @@ def out_to_normal(net_fun, params):
                                                                                     stddev=params['init_sig_var']))
             cov = tf.nn.softplus(tf.matmul(net_out, cov_weights), name=name + '_softplus')
             cov = cov + tf.constant(1.0, dtype=tf.float32, name='min_variance')
-            cov = tf.constant(1.0, dtype=tf.float32, shape=[d_dist], name='fixed_variance')
+            cov = tf.constant(1.0, dtype=tf.float32, shape=[net_out.get_shape()[0], d_dist], name='fixed_variance')
         return mean, cov
     return f
 