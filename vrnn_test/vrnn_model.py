test 41 setup - fixed wrong dim

vrnn_test/vrnn_model.py
@@ -37,7 +37,7 @@ def gaussian_log_p(params_out, x_target, dim):
     # log_x_norm = -0.5 * (dim * tf.log(2 * np.pi) + log_cov_x_det)
     log_x_norm = -0.5 * (dim * 1.83787 + log_cov_x_det)
     log_p = log_x_norm + log_x_exp
-    return log_p, log_x_norm, log_cov_x_det
+    return log_p, log_x_norm, log_x_exp
 
 
 def gm_log_p(params_out, x_target, dim):